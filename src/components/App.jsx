3bcad576416fbc4e161994e622fbf7cd579cb1eb
Ddemkowicz <dakucp@gmail.com>
2023-05-27 14:03:48 +0200

    up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import Section from './Section/Section';
+
 export const App = () => {
   return (
     <div
       style={{
+        backgroundColor: 'rgb(151, 151, 151)',
         height: '100vh',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <Section title="Please leave feedback" />
+      <FeedbackOptions />
     </div>
   );
 };
